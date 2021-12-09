e5451d70faa18c8df4bb5c9033085b16f0756b91
cagriozcaglar <cagriozcaglar@gmail.com>
2021-12-09 13:50:15 -0800

    Added commit commands

---
+++ commit.sh
@@ -0,0 +1,10 @@
+git add .
+message="Added PyTorch Practice Examples"
+# git commit -m "$1"
+git commit -m "$message"
+git push
+
+# randomText="origin`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 6`"
+# git remote add ${randomText} https://github.com/cagriozcaglar/DNNapplicationsWithPyTorch.git
+# git remote -v
+# git push ${randomText} master
